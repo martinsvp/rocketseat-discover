0561d17310d96de2d54009e416bb87827e0656df
Victor Pedro <vpedro45@gmail.com>
2023-11-01 00:21:47 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("toggle-btn");
+
+function toggleMode() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    const img = document.querySelector(".profile img");
+
+    if(html.classList.contains("light")) {
+        img.setAttribute("src", "assets/avatar-light.png");
+    } else {
+        img.setAttribute("src", "assets/avatar.png");
+    }
+}
+
+btn.addEventListener("click", toggleMode);
